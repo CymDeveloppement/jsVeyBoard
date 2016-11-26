c9549dfe65797fb00a611aded874ff6f15b33ee6
Yann Challet <yann@cymdev.com>
2016-11-26 12:39:37 +0100

    Envoie initial

---
+++ jsveyboard.js
@@ -0,0 +1,22 @@
+/*!
+ * jsVeyboard v0.0.1 (https://github.com/CymDeveloppement/jsVeyBoard)
+ * Copyright 2016 SARL CYM DEVELOPPEMENT.
+ * Licensed under the GPL v3
+ */
+
+
+function jsveyboard(identifier, style)
+{
+	//$(identifier).val('OKKKK!');
+	alert(style.btnstyle);
+}
+
+function makeKeyboard(id, style)
+{
+	
+}
+
+function makeButton(name, style)
+{
+	
+}
